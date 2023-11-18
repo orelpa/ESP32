cc894ff1e6d7ef3d70f9e77911ce1196a21fe5ba
orelpa <pa.orel@yandex.ru>
2023-11-18 07:59:23 +0300

    подключение к wi-fi

--- test_wifi/sketch_nov18a/sketch_nov18a.ino
+++ test_wifi/sketch_nov18a/sketch_nov18a.ino
@@ -1,9 +1,22 @@
+#include "WiFi.h" //библиоткеа
+
+//глобальные переменные для хранения имени сети и пароля
+const char* ssid = "home";
+const char* password = "12345678";
+
 void setup() {
-  // put your setup code here, to run once:
+//откроем соединение, что бы выводить результат работы программы
+Serial.begin(115200);
 
+//Вызываем метод begin для объекта WiFi передавая в качестве аргументов SSID и переменную пароля
+WiFi.begin(ssid, password);
+
+//Выполняем цикл пока соединение не будет установлено.
+//Вызываем метод status для объекта WiFi и ждем, пока результат не совпадет с WL_CONNECTED
+//между каждой итерацией выыводим небольшую задержку
+while (WiFi.status() != WL_CONNECTED){
+  delay(500);
+  Serial.println("Connecting to WiFi..");
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
 }
+void loop(){}
