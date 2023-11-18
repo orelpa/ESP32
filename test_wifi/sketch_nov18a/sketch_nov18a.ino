89fdd89064772d6c45a5f59bcf6fa9011dd97a11
orelpa <pa.orel@yandex.ru>
2023-11-18 08:34:47 +0300

    10 попыток подключения вывод ip

--- test_wifi/sketch_nov18a/sketch_nov18a.ino
+++ test_wifi/sketch_nov18a/sketch_nov18a.ino
@@ -4,6 +4,8 @@
 const char* ssid = "home";
 const char* password = "12345678";
 
+byte tries = 10; // попыток подключения
+
 void setup() {
 //откроем соединение, что бы выводить результат работы программы
 Serial.begin(115200);
@@ -14,9 +16,19 @@ WiFi.begin(ssid, password);
 //Выполняем цикл пока соединение не будет установлено.
 //Вызываем метод status для объекта WiFi и ждем, пока результат не совпадет с WL_CONNECTED
 //между каждой итерацией выыводим небольшую задержку
-while (WiFi.status() != WL_CONNECTED){
+while ( --tries && WiFi.status() != WL_CONNECTED){
   delay(500);
-  Serial.println("Connecting to WiFi..");
+  Serial.println(".");
+}
+if (WiFi.status() != WL_CONNECTED){
+  Serial.println("Non Connecting to WiFi..");
+}
+else
+{
+  //Иначе  удалось подключиться и выводим адрес
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println(WiFi.localIP());
 }
 }
 void loop(){}
