6a2cc0f9bae1087cd6c536da036d7dcc14c22451
orelpa <pa.orel@yandex.ru>
2023-11-20 17:06:22 +0300

    убрано лишнее

--- test_wifi/sketch_nov18a/sketch_nov18a.ino
+++ test_wifi/sketch_nov18a/sketch_nov18a.ino
@@ -9,29 +9,42 @@ byte tries = 10; // попыток подключения
 
 
 void setup() {
-//откроем соединение, что бы выводить результат работы программы
-Serial.begin(115200);
+    //откроем соединение, что бы выводить результат работы программы
+    Serial.begin(115200);
 
-//Вызываем метод begin для объекта WiFi передавая в качестве аргументов SSID и переменную пароля
-WiFi.begin(ssid, password);
+    //Вызываем метод begin для объекта WiFi передавая в качестве аргументов SSID и переменную пароля
+    WiFi.begin(ssid, password);
 
-//Выполняем цикл пока соединение не будет установлено.
-//Вызываем метод status для объекта WiFi и ждем, пока результат не совпадет с WL_CONNECTED
-//между каждой итерацией выыводим небольшую задержку
-while ( --tries && WiFi.status() != WL_CONNECTED){
-  delay(500);
-  Serial.println(".");
+    //Объявление пинов с индикаторами
+    uint8_t LED1pin = 4;
+    bool LED1status = LOW;
+    uint8_t LED2pin = 33;
+    bool LED2status = LOW;
+
+    pinMode(LED1pin, OUTPUT);
+    pinMode(LED2pin, OUTPUT);
+
+
+    //Выполняем цикл пока соединение не будет установлено.
+    //Вызываем метод status для объекта WiFi и ждем, пока результат не совпадет с WL_CONNECTED
+    //между каждой итерацией выыводим небольшую задержку
+    while ( --tries && WiFi.status() != WL_CONNECTED){
+      delay(500);
+      Serial.println(".");
+    }
+    if (WiFi.status() != WL_CONNECTED){
+      Serial.println("Non Connecting to WiFi..");
+    }
+    else
+    {
+      //Иначе  удалось подключиться и выводим адрес
+      Serial.println("");
+      Serial.println("WiFi connected");
+      Serial.println(WiFi.localIP());
+    }
+
 }
-if (WiFi.status() != WL_CONNECTED){
-  Serial.println("Non Connecting to WiFi..");
-}
-else
-{
-  //Иначе  удалось подключиться и выводим адрес
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println(WiFi.localIP());
-}
-}
+
+
 void loop(){
 }
