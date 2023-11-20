7463e73334dc07f33ceff357ea8a426b6cf43cbc
orelpa <pa.orel@yandex.ru>
2023-11-20 13:41:49 +0300

    init проверка мигание светодиодом

--- web_server/sketch_nov18b/sketch_nov18b.ino
+++ web_server/sketch_nov18b/sketch_nov18b.ino
@@ -1,4 +1,5 @@
-#include "WiFi.h" //библиоткеа
+#include "WiFi.h" //библиоткеа  wi-fi
+#include <Weberver.h> //библиотке WebServera
 
 //глобальные переменные для хранения имени сети и пароля
 const char* ssid = "home";
@@ -6,11 +7,20 @@ const char* password = "12345678";
 
 byte tries = 10; // попыток подключения
 
+//объявление пинов
+uint8_t LED1pin = 16;
+bool LED1status = LOW;
+uint8_t LED2pin = 17;
+bool LED2status = LOW;
 
 
 void setup() {
 //откроем соединение, что бы выводить результат работы программы
 Serial.begin(115200);
+
+//режим работы пинов
+pinMode (LED1pin, OUTPUT);
+pinMode (LED2pin, OUTPUT);
 
 //Вызываем метод begin для объекта WiFi передавая в качестве аргументов SSID и переменную пароля
 WiFi.begin(ssid, password);
@@ -32,6 +42,13 @@ else
   Serial.println("WiFi connected");
   Serial.println(WiFi.localIP());
 }
+server.on("/", handle_OnConnect);
+server.on("/led1on", handle_led1on);
+server.on("/led1off", nandle_led1off);
+server.on("/led2on", handle_led2on);
+server.on("/led2off", handle_led2off);
+server.begin();
+Serial.println("HTTP server started");
 }
 void loop(){
 }
